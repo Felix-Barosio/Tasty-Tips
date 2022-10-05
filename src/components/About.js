60995d1363602a9bca60d0c092344911a44b3229
Felix <barosiofelix@gmail.com>
2022-10-05 11:54:27 +0300

    Added about section content

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function About() {
     return (
-        <div>About</div>
+        <div className="about">
+            <h2 class="title">About Us</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="https://media.gettyimages.com/vectors/overhead-angle-of-a-person-using-apps-online-for-cooking-vector-id641803276?s=2048x2048" alt='' />
+                </div>
+                <div class="column right">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo velit, magni tenetur eos debitis dolore repellendus distinctio optio quasi iste id ipsam placeat ex vitae modi, ab minima aliquid harum! Ut a accusamus exercitationem perferendis odit aut rem dolorem ex, similique facilis sint vero nesciunt atque cum? Animi, laboriosam odio!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim blanditiis commodi consequatur a adipisci necessitatibus magni eaque. Natus officia necessitatibus voluptatum doloribus nulla id labore, sint architecto laboriosam delectus deserunt corporis maiores quam neque nisi aliquam veritatis tempora exercitationem eos vel! Similique sunt, dolore dicta vel minima facilis et assumenda?</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
