096ba6e281e5111a22a38802bd043f9d58df37ab
Felix <barosiofelix@gmail.com>
2022-10-05 14:34:01 +0300

    Added state and axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 
 import Searchpanel from '../components/Searchpanel';
 
-function Home() {
+const Home = () => {
     return (
         <div>
             <Searchpanel />
