8a85b83d87e2dd731f02df0c4cfc27eefa0db517
Felix <barosiofelix@gmail.com>
2022-10-05 14:48:42 +0300

    Added axios fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,15 @@ import axios from 'axios';
 import Searchpanel from '../components/Searchpanel';
 
 const Home = () => {
+    const [data, setData] = useState([]);
+    const [count, setCount] = useState(0);
+
+    const handleClick = async () => {
+        const res = await axios.get("https://www.themealdb.com/api/json/v1/1/random.php");
+        setData(res.data.meals[0]);
+        setCount(0);
+        console.log(res.data);
+    };
     return (
         <div>
             <Searchpanel />
