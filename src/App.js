18c7141ab3c7531f3de6f1af50a2c8fb2dc8a99b
Felix <barosiofelix@gmail.com>
2022-10-05 10:55:56 +0300

    Added aboutus route and import

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { Routes, Route } from "react-router-dom"
 import './App.css';
 
 import Header from "./components/Header";
+import Aboutus from "./pages/Aboutus";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Routes>
+        <Route exact path="/about" element={<Aboutus />}></Route>
+      </Routes>
     </div>
   );
 }
