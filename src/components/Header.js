fff5a1a17354eb1a8a2762988e6883fad7ac670b
Felix <barosiofelix@gmail.com>
2022-10-05 10:12:52 +0300

    Added header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,26 @@ const Header = () => {
     return (
         <div>
             <div className="row header">
-
+                <div className="navbar">
+                    <div className="navbar-heading">
+                        <h1 activeclassname="active">
+                            <span>Tasty Tips.</span>
+                        </h1>
+                    </div>
+                    <div className="navbar-links">
+                        <ul>
+                            <Link to="/"> {" "}
+                                <li id="home">Home</li>
+                            </Link>
+                            <Link to="/about"> {" "}
+                                <li>About Us</li>
+                            </Link>
+                            <Link to="/contact"> {" "}
+                                <li>Contact</li>
+                            </Link>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
