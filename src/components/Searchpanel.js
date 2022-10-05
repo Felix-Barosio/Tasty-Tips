54b55fa6a7399a3946630e3a5d6c28eacb707c36
Felix <barosiofelix@gmail.com>
2022-10-05 13:12:16 +0300

    Added description on search file

--- src/components/Searchpanel.js
+++ src/components/Searchpanel.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Searchpanel() {
     return (
-        <div>Searchpanel</div>
+        <div>
+            <div className="row">
+                <div className="text-center pt-5 pb-3">
+                    <h2 className='search'>
+                        How to get a random recipe?
+                    </h2>
+                    <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
+                    <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
+                    <p>All you have to do is click the button below </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
