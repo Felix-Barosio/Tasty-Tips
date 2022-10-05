820e57619c60a591f0ae6d9dc413fe25e216164d
Felix <barosiofelix@gmail.com>
2022-10-05 12:11:59 +0300

    Added routing from home

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from "./components/Header";
 import Aboutus from "./pages/Aboutus";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Routes>
+        <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/about" element={<Aboutus />}></Route>
       </Routes>
     </div>
