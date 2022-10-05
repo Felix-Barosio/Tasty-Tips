3a9c0a47f3b43ba268766f1ef2b5a862fface171
Felix <barosiofelix@gmail.com>
2022-10-05 08:56:29 +0300

    Added Components folder

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <>
+            <div className="row header">
+                <div className="navbar">
+                    <div className="navbar-heading">
+                        <h1 className="navLink"
+                            activeclassname="active">
+                            <span>Tasty Tips.</span>
+                        </h1>
+                    </div>
+                    <div className="navbar-links">
+                        <ul>
+                            <Link to="/">
+                                {" "}
+                                <li id="home">Home</li>
+                            </Link>
+                            <Link to="/about">
+                                {" "}
+                                <li>About Us</li>
+                            </Link>
+                            <Link to="/contact">
+                                {" "}
+                                <li>Contact</li>
+                            </Link>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
