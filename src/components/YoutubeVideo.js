51e262d39daf124b71a7479cb8d54aa639f2dad2
Felix <barosiofelix@gmail.com>
2022-10-06 15:43:57 +0300

    Added fix on video

--- src/components/YoutubeVideo.js
+++ src/components/YoutubeVideo.js
@@ -18,6 +18,8 @@ const YoutubeVideo = ({ data }) => {
                                 src={strYoutube.replace("watch?v=", "embed/")}
                                 title={`How to cook ${strMeal}`}
                                 frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
                             ></iframe>
                         ) : null}
                     </div>
