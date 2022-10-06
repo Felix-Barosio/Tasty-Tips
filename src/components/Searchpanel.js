baa28616dc72ab0e6f49be34fb154251c5bcb4e1
Felix <barosiofelix@gmail.com>
2022-10-06 19:09:30 +0300

    Added corrections to file

--- src/components/Searchpanel.js
+++ src/components/Searchpanel.js
@@ -3,15 +3,19 @@ import React from 'react'
 const Searchpanel = ({ onClickSearch }) => {
     function handleClick() { onClickSearch() }
     return (
-        <div>
+        <div className='card' id='list'>
             <div className="row">
                 <div className="text-center pt-5 pb-3">
                     <h2 className='search'>
                         How to get a random recipe?
                     </h2>
-                    <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
-                    <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
-                    <p className='para'>All you have to do is click the button below </p>
+                    <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum
+                        eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus.
+                        Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur
+                        ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis
+                        beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.
+                    </p>
+                    <p className='para'>All you have to do is click the button below.</p>
                 </div>
             </div>
             <div className="row">
@@ -21,7 +25,7 @@ const Searchpanel = ({ onClickSearch }) => {
                         id="search-btn"
                         onClick={handleClick}
                     >
-                        Search Recipe!
+                        Search Recipe.
                     </button>
                 </div>
             </div>
