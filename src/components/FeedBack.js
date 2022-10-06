1667e52518ad78e7159cf50cda9c4f122570a221
Felix <barosiofelix@gmail.com>
2022-10-06 21:01:05 +0300

    Added new message display

--- src/components/FeedBack.js
+++ src/components/FeedBack.js
@@ -10,6 +10,15 @@ const FeedBack = ({ data }) => {
                         <h3 id='youtube'>
                             Give Your Feedback:
                         </h3>
+
+                        <div className="feedback pt-5">
+                            <div id='submision'>
+                                <div>
+                                    <p id='foody'>New Feedback.</p>
+                                </div>
+                                {listOfFeedback}
+                            </div>
+                        </div>
                     </div>
                 ) : null}
             </div>
