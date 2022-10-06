2db86a0f72d41ff0cb411b77c7dfca7a4b4a65d4
Felix <barosiofelix@gmail.com>
2022-10-06 11:06:44 +0300

    Added daclare data values

--- src/components/DispalyFoods.js
+++ src/components/DispalyFoods.js
@@ -1,1 +1,13 @@
+import React from "react";
 
+const DisplayFoods = ({ data }) => {
+    const { strMealThumb, strInstructions, strMeal, strCategory, strArea, strTags, } = data;
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DisplayFoods;
